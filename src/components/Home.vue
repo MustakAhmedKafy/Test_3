326c472f1227dd266fd8741a1ad321205f5bbad8
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2024-03-31 23:43:21 +0600

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Welcome to Our Cafe!</h1>
+      <button @click="goToMenu" class="btn btn-warning">View Menu</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goToMenu = () => {
+  router.push("/menu");
+};
+</script>
+
+<style scoped>
+.container{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="container">
+      <h2>Menu Item</h2>
+     <ul class="list-group">
+      <li v-for="item in menuItems" :key="item.id" class="list-group-item">
+        <button @click="showAlert(item.name)" class="btn btn-success w-100">{{ item.name }}</button>
+      </li>
+    </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const menuItems = [
+  { id: 1, name: "Coffee" },
+  { id: 2, name: "Tea" },
+  { id: 3, name: "Sandwich" },
+  { id: 4, name: "Cake" },
+];
+
+const showAlert = (itemName) => {
+  alert(`You selected ${itemName}`);
+};
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
